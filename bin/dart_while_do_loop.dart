0da23025f8ee002af00efa7b24b2a5b963badd95
Nurul Islam <nurul.cse7@gmail.com>
2025-03-14 19:21:26 +0600

    added dart while do loop

---
+++ bin/dart_while_do_loop.dart
@@ -0,0 +1,19 @@
+void main(){
+
+  // for(int i=0; i<5; i++){
+  //   print("Interaction $i");
+  // }
+  
+ int i = 0;
+ while(i < 5){
+   print("Interaction: $i");
+   i++;
+ }
+
+int j = 0;
+ do{
+   print("Interaction: $j");
+   j++;
+ }while(j <3);
+
+}
