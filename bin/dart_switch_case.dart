8a0edaa5d42192537050a972e33a3f73c428e94c
Nurul Islam <nurul.cse7@gmail.com>
2025-03-14 17:21:40 +0600

    Dart switch case file added

---
+++ bin/dart_switch_case.dart
@@ -0,0 +1,23 @@
+void main(){
+  String grade = "A+";
+
+  switch(grade){
+
+    case "A+":
+    print("Exelent");
+    break;
+
+    case "A":
+    print("Good");
+    break;
+
+    case "A-":
+    print("Average");
+    break;
+
+    default:
+      print("Invilid Input");
+
+
+  }
+}
