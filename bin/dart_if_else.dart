88daa4c6fb65287019e58244fc077f31660c51ad
Nurul Islam <nurul.cse7@gmail.com>
2025-03-14 16:04:46 +0600

    Dart if else file added

---
+++ bin/dart_if_else.dart
@@ -0,0 +1,19 @@
+// void main (){
+//   int mathMark = 85;
+
+//   if(mathMark>=80){
+//     print("A+");
+//   }else {
+//     print("A"); 
+//   }
+// }
+// // A+ 
+
+void main(){
+  int age = 20;
+  if (age>=18){
+    print("You can apply for NID");
+  }else{
+    print("You are not eligible for NID");
+  }
+}
