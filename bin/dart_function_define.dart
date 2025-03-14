30240bec00f09475fe37947269778ba18ba22c3f
Nurul Islam <nurul.cse7@gmail.com>
2025-03-14 19:33:27 +0600

    added dart function define

---
+++ bin/dart_function_define.dart
@@ -0,0 +1,31 @@
+// // Basic function
+// void greet () {
+//   print("Hello");
+// }
+
+// void main (){
+//   greet();
+// }
+// //********************** */
+
+// // argument bypass 
+// void greet (String name) {
+//   print("Hello $name");
+// }
+
+// void main (){
+//   greet("Nurul");
+//   greet("Islam");
+
+// }
+// //********************** */
+
+
+int addNumber(int a, int b){
+  return a+b;
+}
+
+void main(){
+  int result = addNumber(5, 7);
+  print(result);
+}
